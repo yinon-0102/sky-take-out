4dcad7271d33b91add264fe333784ceb92a37450
a01c02 <2720219284@qq..com>
2026-03-19 20:20:43 +0800

    分类管理

--- sky-common/src/main/java/com/sky/properties/AliOssProperties.java
+++ sky-common/src/main/java/com/sky/properties/AliOssProperties.java
@@ -5,13 +5,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "sky.alioss")
+@ConfigurationProperties(prefix = "aliyun.oss")
 @Data
 public class AliOssProperties {
 
     private String endpoint;
-    private String accessKeyId;
-    private String accessKeySecret;
     private String bucketName;
+    private String region;
 
 }
--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -1,10 +1,41 @@
 package com.sky.controller.admin;
 
 
+import com.sky.constant.MessageConstant;
+import com.sky.result.Result;
+import com.sky.utils.AliyunOSSOperator;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-@RequestMapping
-@RestController("/admin/common")
+import java.io.IOException;
+
+@Slf4j
+@RequestMapping("/admin/common")
+@RestController
 public class CommonController {
+
+
+    @Autowired
+    private AliyunOSSOperator aliyunOSSOperator;
+
+    /**
+     * 文件上传
+     */
+    @PostMapping("/upload")
+    public Result upload(MultipartFile file) throws Exception {
+        log.info("文件上传:{}", file.getOriginalFilename());
+        //将文件交给OSS管理
+        try {
+            String url = aliyunOSSOperator.upload(file.getBytes(), file.getOriginalFilename());
+            log.info("文件上传OSS,url:{}", url);
+            return Result.success(url);
+        } catch (IOException e) {
+            log.error("文件上传失败: {}", e);
+        }
+        return Result.error(MessageConstant.UPLOAD_FAILED);
+    }
 }
