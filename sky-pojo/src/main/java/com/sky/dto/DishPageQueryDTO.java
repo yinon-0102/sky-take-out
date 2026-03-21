9b0893391dc80899cf63cad33fc700fc143e0ef5
a01c02 <2720219284@qq..com>
2026-03-21 17:53:58 +0800

    update

--- sky-pojo/src/main/java/com/sky/dto/DishPageQueryDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/DishPageQueryDTO.java
@@ -12,10 +12,8 @@ public class DishPageQueryDTO implements Serializable {
     private int pageSize;
 
     private String name;
-
     //分类id
     private Integer categoryId;
-
     //状态 0表示禁用 1表示启用
     private Integer status;
 
--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,14 +1,16 @@
 package com.sky.controller.admin;
 
+import com.github.pagehelper.PageHelper;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.dto.DishDTO;
 import com.sky.service.DishService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -24,6 +26,20 @@ public class DishController {
     public Result save(@RequestBody DishDTO dishDTO) {
         log.info("新增菜品：{}", dishDTO);
         dishService.saveWithFlavor(dishDTO);
+        return Result.success();
+    }
+
+    @GetMapping("/page")
+    public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO){
+        log.info("分页查询：{}", dishPageQueryDTO);
+        PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    @DeleteMapping
+    public Result delete(@RequestParam List<Long> ids){
+        log.info("删除菜品：{}", ids);
+        dishService.deleteBatch(ids);
         return Result.success();
     }
 }
