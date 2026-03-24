4545442b5c04fe5c0edece2b5d30b2d7f647321c
a01c02 <2720219284@qq..com>
2026-03-24 16:10:27 +0800

    新增C端用户模块相关接口
    - 新增CategoryController、DishController、SetmealController、UserController
    - 新增JwtTokenUserInterceptor用户端JWT拦截器
    - 新增UserMapper、SetmealMapper及对应XML
    - 新增SetmealService、UserService及实现类
    - 修改DishServiceImpl、SetmealServiceImpl用户端查询逻辑
    - 更新WebMvcConfiguration注册用户端拦截器

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -25,6 +25,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -138,5 +139,40 @@ public class DishServiceImpl implements DishService {
         }
         dishFlavorMapper.insertBatch(flavors);
     }
+    /**
+     * 条件查询菜品和口味
+     * @param dish
+     * @return
+     */
+    public List<DishVO> listWithFlavor(Dish dish) {
+        List<Dish> dishList = dishMapper.list(dish);
 
+        List<DishVO> dishVOList = new ArrayList<>();
+
+        for (Dish d : dishList) {
+            DishVO dishVO = new DishVO();
+            BeanUtils.copyProperties(d,dishVO);
+
+            //根据菜品id查询对应的口味
+            List<DishFlavor> flavors = dishFlavorMapper.getByDishId(d.getId());
+
+            dishVO.setFlavors(flavors);
+            dishVOList.add(dishVO);
+        }
+
+        return dishVOList;
+    }
+
+    /**
+     * 根据分类id查询菜品
+     * @param categoryId
+     * @return
+     */
+    public List<Dish> list(Long categoryId) {
+        Dish dish = Dish.builder()
+                .categoryId(categoryId)
+                .status(StatusConstant.ENABLE)
+                .build();
+        return dishMapper.list(dish);
+    }
 }
---
+++ sky-server/src/main/java/com/sky/service/impl/UserServiceImpl.java
@@ -0,0 +1,77 @@
+package com.sky.service.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.sky.constant.MessageConstant;
+import com.sky.dto.UserLoginDTO;
+import com.sky.entity.User;
+import com.sky.exception.LoginFailedException;
+import com.sky.mapper.UserMapper;
+import com.sky.properties.WeChatProperties;
+import com.sky.service.UserService;
+import com.sky.utils.HttpClientUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import javax.security.auth.login.LoginException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Service
+public class UserServiceImpl implements UserService {
+
+    //微信服务接口地址
+    public static final String WX_LOGIN = "https://api.weixin.qq.com/sns/jscode2session";
+
+    @Autowired
+    private WeChatProperties weChatProperties;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    /**
+     * 微信登录
+     * @param loginDTO
+     * @return
+     */
+    public User wxLogin(UserLoginDTO loginDTO) {
+        //调用微信接口服务获取openid
+        String openid=getOpenid(loginDTO.getCode());
+        //判断openid是否为空,空则抛异常
+        if (openid == null){
+            throw new LoginFailedException(MessageConstant.LOGIN_FAILED);
+        }
+        //判断当前用户是否为新用户
+        User user = userMapper.getByOpenid(openid);
+        // 新用户则自动完成注册
+        if (user == null){
+            user = User.builder()
+                    .openid(openid)
+                    .createTime(LocalDateTime.now())
+                    .build();
+            userMapper.insert(user);
+        }
+        //返回用户对象
+        return user;
+    }
+
+    /**
+     * 调用微信接口服务，获取openid
+     * @param code
+     * @return
+     */
+    private String getOpenid(String code) {
+        Map<String, String> map = new HashMap<>();
+        map.put("appid", weChatProperties.getAppid());
+        map.put("secret", weChatProperties.getSecret());
+        map.put("js_code", code);
+        map.put("grant_type", "authorization_code");
+        String json = HttpClientUtil.doGet(WX_LOGIN, map);
+
+        JSONObject jsonObject = JSON.parseObject(json);
+        String openid = jsonObject.getString("openid");
+        return openid;
+    }
+}
